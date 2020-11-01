eaabda79e81d84b932f2e7b0716d207bc514c947
牧之瀬樹 <65141237+mknsttk@users.noreply.github.com>
2020-11-02 07:53:24 +0900

    Create index.js
    webpack entry point

---
+++ index.js
@@ -0,0 +1,12 @@
+import "@babel/polyfill";
+
+import './scripts/gsap';
+import './scripts/scrollTrigger';
+import './scripts/swiper';
+import './scripts/microModal';
+import './scripts/dropDown';
+
+import './style/index.scss';
+
+
+
